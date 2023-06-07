a838cfaf8b6ab7d45cbf140f2135792a1ae19c00
Lina Vorpahl <115728991+THE-BARN-Lina@users.noreply.github.com>
2023-06-07 16:58:40 +0200

    Add files via upload

---
+++ appstle-init.js
@@ -0,0 +1,160 @@
+(function (window, k) {
+    if (!window.AppstleIncluded && (!urlIsProductPage() || 'V1' === 'V2')) {
+      window.AppstleIncluded = true;
+      appstleLoadScript = function (src, callback) {
+        var script = document.createElement("script");
+        script.charset = "utf-8";
+            script.async = true;
+        script.src = src;
+        script.onload = script.onreadystatechange = function () {
+          if (!script.readyState || /loaded|complete/.test(script.readyState)) {
+            script.onload = script.onreadystatechange = null;
+            script = k;
+            callback && callback();
+          }
+        };
+            document.getElementsByTagName("head")[0].appendChild(script)
+      };
+
+
+      appstleLoadScript("https://cdn.shopify.com/s/files/1/0643/2224/0731/t/1/assets/appstle-subscription.js?v=1651238520");
+
+      window.RS = Window.RS || {};
+      RS.Config = {
+        "selectors": {
+            "payment_button_selectors": "form[action$='/cart/add'] .shopify-payment-button",
+            "subscriptionLinkSelector": "#MainContent > div > div:nth-child(2) > div:nth-child(2) > h2",
+            "atcButtonPlacement": "BEFORE",
+            "subscriptionLinkPlacement": "BEFORE",
+            "cartRowSelector": "",
+            "cartLineItemSelector": "",
+            "cartLineItemPerQuantityPriceSelector": "",
+            "cartLineItemTotalPriceSelector": "",
+            "cartLineItemSellingPlanNameSelector": "",
+            "cartSubTotalSelector" : "",
+            "cartLineItemPriceSelector": "",
+        },
+        "enableCartWidgetFeature": "false",
+        "useUrlWithCustomerId": "false",
+        "atcButtonSelector": "div.product-form__buttons",
+        "moneyFormat": "{% raw %}${{amount}}{% endraw %}",
+        "oneTimePurchaseText": "One Time Purchase",
+        "shop": "the-barn-north-america.myshopify.com",
+        "deliveryText": "delivery",
+        "purchaseOptionsText": "",
+        "manageSubscriptionButtonText": "Manage Subscription",
+        "subscriptionOptionText": "Subscription Service",
+        "sellingPlanSelectTitle": "SCHEDULE",
+        "subscriptionPriceDisplayText": "",
+        "tooltipTitle": "",
+        "api_key": "",
+        "showTooltipOnClick": "false",
+        "tooltipDesctiption": "<strong>Seamless subscription your way<\/strong><br>Skip, reschedule, edit, or cancel deliveries anytime, based on your needs.",
+        "tooltipDescriptionOnPrepaidPlan": "<b>Prepaid Plan Details<\/b><\/br> Total you are charged for {{totalPrice}} ( At every order {{pricePerDelivery}})",
+        "tooltipDescriptionOnMultipleDiscount": "<b>Discount Details<\/b><\/br> Initial Discount is {{discountOne}} and then {{discountTwo}}",
+        "tooltipDescriptionCustomization": "",
+        "orderStatusManageSubscriptionTitle": "Subscription",
+        "orderStatusManageSubscriptionDescription": "Continue to your account to view and manage your subscriptions. Please use the same email address that you used to buy the subscription.",
+        "orderStatusManageSubscriptionButtonText": "Manage your subscription",
+        "subscriptionOptionSelectedByDefault" : true,
+        "totalPricePerDeliveryText" : "{{prepaidPerDeliveryPrice}}\/delivery",
+        "memberOnlySellingPlansJson": {},
+        "nonMemberOnlySellingPlansJson": {},
+        "widgetEnabled": true,
+        "showTooltip" : false,
+        "sortByDefaultSequence": false,
+        "showSubOptionBeforeOneTime": false,
+        "showStaticTooltip": false,
+        "showAppstleLink": false,
+        "sellingPlanTitleText" : "{{sellingPlanName}} ({{sellingPlanPrice}}\/delivery)",
+        "oneTimePriceText" : "{{price}}",
+        "selectedPayAsYouGoSellingPlanPriceText" : "{{price}}",
+        "selectedPrepaidSellingPlanPriceText" : " {{totalPrice}}",
+        "selectedDiscountFormat" : "SAVE {{selectedDiscountPercentage}}",
+        "manageSubscriptionBtnFormat" : "<a href='apps\/subscriptions' class='appstle_manageSubBtn' ><button class='btn' style='padding: 2px 20px'>Manage Subscription<\/button><a><br><br>",
+        "manageSubscriptionUrl" : "apps\/subscriptions",
+        "appstlePlanId": 3,
+        "showCheckoutSubscriptionBtn": true,
+        "disableLoadingJquery": false,
+        "widgetEnabledOnSoldVariant": "false",
+        "switchRadioButtonWidget": false,
+        "appstlePlanName": "BUSINESS",
+        "formMappingAttributeName": "",
+        "formMappingAttributeSelector": "",
+        "quickViewModalPollingSelector": "",
+        "scriptLoadDelay": "0",
+        "formatMoneyOverride": "false",
+        "appstle_app_proxy_path_prefix": "apps\/subscriptions",
+        "updatePriceOnQuantityChange": "",
+        "widgetParentSelector": "",
+        "quantitySelector": "",
+        "css": {
+            "appstle_subscription_widget": {
+                "margin-top": "" ,
+                "margin-bottom": "30px",
+            },
+
+            "appstle_subscription_wrapper": {
+                "border-width": "2px",
+                "border-color": "#888888",
+            },
+
+            "appstle_circle": {
+                "border-color": "#68431b",
+            },
+
+            "appstle_dot": {
+                "background-color": "#68431b",
+            },
+
+            "appstle_select": {
+                "padding-top": "",
+                "padding-bottom": "",
+                "padding-left": "",
+                "padding-right": "",
+                "border-width": "",
+                "border-style": "",
+                "border-color": "",
+                "border-radius": "",
+            },
+
+            "tooltip_subscription_svg": {
+                "fill": "",
+            },
+
+            "appstle_tooltip": {
+                "color": "",
+                "background-color": "",
+            },
+
+            "appstle_tooltip_border_top_color": {
+                "border-top-color": "",
+            },
+
+            "appstle_subscription_final_price": {
+                "color": "",
+            },
+            "appstle_widget_text_color": {
+                "color": "",
+            },
+            "appstle_selected_background": {
+                "background": "transparent",
+            },
+            "customCSS": "",
+            "customerPortalCss": "",
+            "priceSelector": "span.price-item.price-item--regular",
+            "landingPagePriceSelector": "",
+            "quickViewClickSelector": "",
+            "badgeTop": "",
+            "pricePlacement": "BEFORE"
+        }
+      };
+
+    }
+
+    function urlIsProductPage() {
+    // return null != decodeURIComponent(window.location.pathname).match(/\/products\/(([a-zA-Z0-9]|[\-\.\_\~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[\ud83c\ud83d\ud83e][\ud000-\udfff]){1,})\/?/)
+    return decodeURIComponent(window.location.pathname).includes('/products/');
+    }
+  }
+)(window);
